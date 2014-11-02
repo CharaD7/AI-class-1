c9abf24394224d51707c45db76254bb72bdb15e9
ahmed-elhelow <ahmed.elhelow@90daraga.com>
2014-11-02 23:15:34 +0200

    Sudoku
    Sudoku by python

---
+++ sudoku-section.py
@@ -0,0 +1,130 @@
+from copy import deepcopy
+
+Grid = [['.', '.', '2', '3'],
+        ['.', '.', '.', '.'],
+        ['.', '.', '.', '.'],
+        ['3', '4', '.', '.']]
+
+square_indx = [[(0,2),(0,2)],
+               [(0,2),(2,4)],
+               [(2,4),(0,2)],
+               [(2,4),(2,4)]]
+
+elements = ['1','2','3','4']
+
+def get_squares(new_Grid):
+    squares = []
+    for i in range(0,4):
+        square = {}
+        for x in range(square_indx[i][0][0],square_indx[i][0][1]):
+            for y in range(square_indx[i][1][0],square_indx[i][1][1]):
+                square[(x,y)] = new_Grid[x][y]
+        squares.append(square)
+    return squares
+
+def get_rows(new_Grid):
+    rows = []
+    for i in range(0,4):
+        row = {}
+        for y in range(0,4):
+            row[(i,y)] = new_Grid[i][y]
+        rows.append(row)
+    return rows
+
+def get_cols(new_Grid):
+    cols = []
+    for i in range(0,4):
+        col = {}
+        for x in range(0,4):
+            col[(x,i)] = new_Grid[x][i]
+        cols.append(col)
+    return cols
+    
+def get_all_related_cells(new_Grid):
+    squares = get_squares(new_Grid)
+    rows = get_rows(new_Grid)
+    cols = get_cols(new_Grid)
+    all_vec = squares + rows + cols
+    return all_vec
+    
+def get_legal_for_cell(cell_r, cell_c, new_Grid):
+    map = {}
+    all_vec = get_all_related_cells(new_Grid)
+    for a in all_vec:
+        if (cell_r, cell_c) in a.keys():
+            del a[(cell_r, cell_c)]
+            map.update(a)
+
+    exist = []
+    for m in map:
+        if not map[m]=='.':
+            exist.append(map[m])
+
+    rest = list(set(elements) - set(exist))
+    return rest
+
+def get_new_r_c(r, c):
+    if c==3:
+        if r==3:
+            new_r = r
+            new_c = c
+        else:
+            new_c = 0
+            new_r = r+1
+    else:
+        new_r = r
+        new_c = c+1
+    return new_r, new_c
+
+def is_complete(new_Grid):
+    grid_complete = True
+    for r in new_Grid:
+        grid_complete = grid_complete and not ('.' in r)
+    return grid_complete
+
+def print_grid(new_Grid):
+    for item in new_Grid:
+        print item
+    print
+
+def solve_step_in_sudoko(last_Grid, r, c):
+    new_Grid = deepcopy(last_Grid)
+    
+    complete = False        # complete flag initialization
+    unknown_cell = True     # unknown_cell flag initialization
+    
+    if is_complete(new_Grid):
+        complete = True
+    
+    if new_Grid[r][c]!='.':
+        unknown_cell = False
+        legal_for_cell = [None]
+    else:
+        legal_for_cell = get_legal_for_cell(r, c, new_Grid)
+    
+    for item in legal_for_cell:
+        if complete:
+            break
+        if unknown_cell:
+            new_Grid[r][c] = item
+        new_r, new_c = get_new_r_c(r, c)
+
+        returned_Grid = solve_step_in_sudoko(new_Grid, new_r, new_c)
+        if not returned_Grid==None:
+            complete = True     # to deliver returned value from /
+                                # deep function to current one
+            new_Grid = returned_Grid
+
+    if complete:
+        return new_Grid
+
+print_grid(solve_step_in_sudoko(Grid, 0, 0))
+                
+
+##for s in squares:   
+##    print s
+
+##print get_legal_for_cell(0, 0, Grid)
+##all_related_cells = get_all_related_cells(Grid)
+##for a in all_related_cells:
+##    print a
